68a12504367f4f0c60c6e84f3d22eba79bcfc812
ngtv <tuanvy860@gmail.com>
2026-01-08 19:28:46 +0700

    feat: change to XDG_HOME_DIR and HOME/.local/share rather than config. Implement dir setup

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,17 @@
 use std::env;
-use std::path::{PathBuf};
+use std::path::{PathBuf, Path};
+use std::fs;
 
 use anyhow::{Result as AnyResult, Context};
 
 fn main() -> AnyResult<()> {
-    let theme_dir = get_config_dir().context(
+    let theme_dir : PathBuf = get_config_dir().context(
             concat!(
             "Error: Cannot decide config directory. (expects as least one of ",
-            "EZA_THEME_DIR, XDG_CONFIG_PATH, HOME to be set)")
+            "EZA_THEME_DIR, XDG_DATA_HOME, HOME to be set)")
         )?;
+    let theme_dir: &Path = theme_dir.as_path();
+    fs::create_dir_all(theme_dir)?;
 
     println!("{theme_dir:?}");
     
@@ -18,13 +21,13 @@ fn main() -> AnyResult<()> {
 fn get_config_dir() -> Option<PathBuf> {
     env::var("EZA_THEME_DIR").ok().map(PathBuf::from)
         .or_else(|| {
-            env::var("XDG_CONFIG_PATH")
+            env::var("XDG_DATA_HOME")
                 .ok()
                 .map(|p| PathBuf::from(p).join("eza-themes"))
         })
         .or_else(|| {
             env::var("HOME")
                 .ok()
-                .map(|p| PathBuf::from(p).join(".config").join("eza-themes"))
+                .map(|p| PathBuf::from(p).join(".local").join("share").join("eza-themes"))
         })
 }
