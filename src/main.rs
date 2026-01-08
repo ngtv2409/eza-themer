9fb5b4a0d185e45997855d19e579a197579a579f
ngtv <tuanvy860@gmail.com>
2026-01-08 20:03:44 +0700

    feat: implement get eza dir

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use std::env;
 use std::path::{PathBuf, Path};
 use std::fs;
 
-use anyhow::{Result as AnyResult, Context};
+use anyhow::{Result as AnyResult, Context, anyhow};
 use clap::Parser;
 
 fn main() -> AnyResult<()> {
@@ -16,6 +16,21 @@ fn main() -> AnyResult<()> {
             "EZA_THEME_DIR, XDG_DATA_HOME, HOME to be set)")
         )?;
     let theme_dir: &Path = theme_dir.as_path();
+    let eza_dir : PathBuf = get_eza_dir().context(
+            concat!(
+            "Error: Cannot decide eza directory. (expects as least one of ",
+            "EZA_CONFIG_DIR, XDG_CONFIG_HOME, HOME to be set)")
+        )?;
+    let eza_dir: &Path = theme_dir.as_path();
+    if ! eza_dir.exists() {
+        return Err(anyhow!(
+            concat!(
+                "Eza home does not exists at designated location ",
+                "or is not a directory. ",
+                "Refuse to create."
+            )
+        ));
+    }
     fs::create_dir_all(theme_dir)?;
 
     println!("{theme_dir:?}");
@@ -34,5 +49,19 @@ fn get_config_dir() -> Option<PathBuf> {
             env::var("HOME")
                 .ok()
                 .map(|p| PathBuf::from(p).join(".local").join("share").join("eza-themes"))
+        })
+}
+
+fn get_eza_dir() -> Option<PathBuf> {
+    env::var("EZA_CONFIG_DIR").ok().map(PathBuf::from)
+        .or_else(|| {
+            env::var("XDG_CONFIG_HOME")
+                .ok()
+                .map(|p| PathBuf::from(p).join("eza"))
+        })
+        .or_else(|| {
+            env::var("HOME")
+                .ok()
+                .map(|p| PathBuf::from(p).join(".config").join("eza"))
         })
 }
