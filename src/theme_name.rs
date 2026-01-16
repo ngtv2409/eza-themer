bf41da56d6a92c73da33f9778e9f11e6eec48116
ngtv <tuanvy860@gmail.com>
2026-01-16 11:34:03 +0700

    feat: implement theme name functionalities

---
+++ src/theme_name.rs
@@ -0,0 +1,71 @@
+/// Validate amd canonicallize theme name
+use std::path::PathBuf;
+
+#[derive(Debug)]
+pub struct ThemeName {
+    s: String
+}
+
+impl ThemeName {
+    /// creates a canonical theme name string
+    pub fn from_str(s: &str) -> Option<Self> {
+        if !is_valid_theme_name(s) {
+            return None;
+        }
+        let s: String = canon_sep(&s.to_ascii_lowercase());
+        if s.is_empty() {
+            return None;
+        }
+
+        Some(Self {
+            s: s
+        })
+    }
+
+    pub fn prettify(&self) -> String {
+         self.s.split('-')
+             .map(|word| {
+                 let mut chars = word.chars();
+                 match chars.next() {
+                     Some(first) => first.to_uppercase().collect::<String>() + chars.as_str(),
+                     None => String::new(),
+                 }
+             })
+             .collect::<Vec<_>>()
+             .join(" ")
+    }
+
+    pub fn to_filename(&self) -> PathBuf {
+        let p = PathBuf::from(&self.s);
+        p.with_extension("yml")
+    }
+}
+
+fn canon_sep(input: &str) -> String {
+    let mut out = String::with_capacity(input.len());
+    let mut last_was_sep = false;
+
+    for c in input.chars() {
+        let is_sep = c == '-' || c == '_' || c == ' ';
+
+        if is_sep {
+            if !last_was_sep {
+                out.push('-');
+                last_was_sep = true;
+            }
+        } else {
+            out.push(c);
+            last_was_sep = false;
+        }
+    }
+
+    out.trim_matches('-').to_string()
+}
+
+fn is_valid_theme_name(s: &str) -> bool {
+    s.bytes().all(|b| {
+        matches!(b,
+            b'a'..=b'z' | b'A'..=b'Z' | b'0'..=b'9' | b'_' | b'-' | b' '
+        )
+    })
+}
